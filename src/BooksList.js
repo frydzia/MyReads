f1128bf90a248c84aeffa4eb5ae25ee212bb9684
Jola Kasperek <jolakasperek@gmail.com//>
2018-07-16 21:55:21 +0200

    add ShelfChanger component

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ShelfChanger from "./ShelfChanger";
 
 class BooksList extends Component {
   render() {
@@ -9,7 +10,8 @@ class BooksList extends Component {
             <div key={book.id} className="book">
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                <div className="book-shelf-changer"></div>
+                <ShelfChanger
+                  shelf={this.props.currentShelf} />
               </div>
               <div className="book-title">{book.title}</div>
               <div className="book-authors">{book.author}</div>
--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
+class ShelfChanger extends Component {
+  state = { currentShelf: '' }
 
-<select>
-  <option value="move" disabled>Move to...</option>
-  <option value="currentlyReading">Currently Reading</option>
-  <option value="wantToRead">Want to Read</option>
-  <option value="read">Read</option>
-  <option value="none">None</option>
-</select>
+  handleChange = (event) => {
+    this.setState({ currentShelf: event.target.value })
+  }
+
+  render() {
+    return(
+      <div className="book-shelf-changer">
+        <select onChange={this.handleChange}>
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+export default ShelfChanger
