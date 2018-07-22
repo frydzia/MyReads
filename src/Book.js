5f4fbc7e1946305ebb372fef59c5243a8ee061cf
Jola Kasperek <jolakasperek@gmail.com//>
2018-07-23 00:14:33 +0200

    add destructuring

--- src/Book.js
+++ src/Book.js
@@ -9,21 +9,23 @@ class Book extends Component {
   };
 
   render() {
+    const { book, moveBooksToNewShelf } = this.props
+
     // set black image for books with no cover
-    const cover = this.props.book.imageLinks && this.props.book.imageLinks.thumbnail ? this.props.book.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/180px-Black_colour.jpg"
+    const cover = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/180px-Black_colour.jpg"
 
     return (
       <div>
-        <div id={this.props.book.id} className="book">
+        <div id={book.id} className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${cover}")` }}></div>
             <ShelfChanger
-              book={this.props.book}
-              moveBooksToNewShelf={this.props.moveBooksToNewShelf}
+              book={book}
+              moveBooksToNewShelf={moveBooksToNewShelf}
           />
           </div>
-          <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.author}</div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.author}</div>
         </div>
       </div>
     )
