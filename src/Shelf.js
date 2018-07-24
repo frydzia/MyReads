0c1a0d196faadda516b6248473f9d693154c8c5a
Jola Kasperek <jolakasperek@gmail.com//>
2018-07-24 12:04:14 +0200

    change Class Component to Stateless Functional Component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book.js';
 import PropTypes from "prop-types";
 
-class Shelf extends Component {
-  static propTypes = {
-    moveBooksToNewShelf: PropTypes.func.isRequired
-  }
-
-  render() {
+const Shelf = (props) => {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.map((book) => (
+            {props.books.map((book) => (
               <li key={book.id}>
                 <Book
                   book={book}
-                  moveBooksToNewShelf={this.props.moveBooksToNewShelf}
+                  moveBooksToNewShelf={props.moveBooksToNewShelf}
                 />
               </li>
             ))}
@@ -25,7 +20,10 @@ class Shelf extends Component {
         </div>
       </div>
     )
-  }
+}
+
+Shelf.propTypes = {
+  moveBooksToNewShelf: PropTypes.func.isRequired
 }
 
 export default Shelf
